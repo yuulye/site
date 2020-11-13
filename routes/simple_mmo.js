fa795482ee2138486228e073d5466b9227702fe3
yuulye <yuulye@gmail.com>
2020-11-13 18:18:17 +0700

    adding steps

--- routes/simple_mmo.js
+++ routes/simple_mmo.js
@@ -37,10 +37,13 @@ router.get('/', async function(req, res, next) {
     .then(body => {
       const $ = cheerio.load(body);
       const name = $(".kt-user-card__name").text().split('\n');
+      const attrs = $(".kt-widget31__item");
+      const steps = $($(attrs.get(7)).find("span").get(1)).text();
       const player = {
         id: o,
         name: name[4],
         level: Number(name[5].split(' ')[1]),
+        steps: steps,
       };
       data.players.push(player);
     });
@@ -48,6 +51,28 @@ router.get('/', async function(req, res, next) {
   /*
   console.log(data.players);
   */
+
+  /*
+  data.players = [
+  { id: 497281, name: '[PTB] yuulye ', level: 36, steps: '1,005' },
+  { id: 496228, name: '[PTB] L33Kx  ', level: 36, steps: '1,278' },
+  { id: 497363, name: '[PTB] Parakalix  ', level: 34, steps: '1,157' },
+  { id: 495485, name: '[PTB] Jadus Katarn ', level: 25, steps: '775' },
+  { id: 496412, name: '[PTB] Van  ', level: 37, steps: '1,387' },
+  { id: 496003, name: '[PTB] Griffin ', level: 17, steps: '483' },
+  { id: 497367, name: '[PTB] Ťhįçç ', level: 21, steps: '850' }
+];
+
+  data.players = [
+  { id: 497281, name: '[PTB] yuulye ', level: 36},
+  { id: 496228, name: '[PTB] L33Kx  ', level: 36},
+  { id: 497363, name: '[PTB] Parakalix  ', level: 34},
+  { id: 495485, name: '[PTB] Jadus Katarn ', level: 25},
+  { id: 496412, name: '[PTB] Van  ', level: 37},
+  { id: 496003, name: '[PTB] Griffin ', level: 17},
+  { id: 497367, name: '[PTB] Ťhįçç ', level: 21}
+];
+*/
 
   res.render('smmo', data);
 });
